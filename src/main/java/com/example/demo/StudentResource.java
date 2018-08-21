2109884217e80fc806ca76a92313c89d730916d4
Karunesh Singh <karuneshsingh@bhavnacorp.com>
2018-08-21 13:29:49 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/StudentResource.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentResource {
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@GetMapping("/students")
+	public List<Student> retrieveAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	@GetMapping("/students/{id}")
+	public Student retrieveStudent(@PathVariable long id) {
+		return studentRepository.findById(id);
+	}
+
+	
+}
